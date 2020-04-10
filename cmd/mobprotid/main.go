b806fdb9748c786adfc522c01094bc7f0a38e9d1
Nikola <nikola@nglabs.local>
2020-04-10 22:08:44 +0100

    Created initial go files for core components and created internal packages

---
+++ cmd/mobprotid/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/arbitraryrw/MobProtID/internal/app/engine"
+)
+
+func main() {
+	fmt.Println("MobProtID Starting..")
+	fmt.Println(engine.Description())
+}
