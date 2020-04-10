59e0a976b01efb9b63c349f7d58846c56f254f5e
Nikola <nikola@nglabs.local>
2020-04-10 22:19:47 +0100

    Setting up initial component references / fixing gitignore

--- cmd/mobprotid/main.go
+++ cmd/mobprotid/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/arbitraryrw/MobProtID/internal/app/engine"
+
+	"github.com/arbitraryrw/MobProtID/internal/pkg/utils"
 )
 
 func main() {
 	fmt.Println("MobProtID Starting..")
 	fmt.Println(engine.Description())
+	fmt.Println(utils.Description())
 }
