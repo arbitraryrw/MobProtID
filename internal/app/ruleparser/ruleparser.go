35630af4c1b1fa66d64279c08194a58b67f22958
Nikola <nikola@nglabs.local>
2020-04-11 02:04:23 +0100

    Playing around with json parsing in go, will revise parsing once json format is finalised

--- internal/app/ruleparser/ruleparser.go
+++ internal/app/ruleparser/ruleparser.go
@@ -1,7 +1,28 @@
 package ruleparser
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func ParseRuleFile() {
 	fmt.Println("Parsing rule file..")
+
+	jsonFile, err := os.Open("/home/nikola/projects/MobProtID/rules/test.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	fmt.Println(result["users"])
 }
