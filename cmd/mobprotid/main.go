8b291f5603bf6f2d7f7efafcfd32ae764ba8782b
arbitraryrw <nickcucakovic@gmail.com>
2020-04-12 21:54:12 +0100

    Moving POC code from main.go file to engine.go to separate the logic

--- cmd/mobprotid/main.go
+++ cmd/mobprotid/main.go
@@ -5,22 +5,12 @@ import (
 
 	"github.com/arbitraryrw/MobProtID/internal/app/engine"
 
-	"github.com/arbitraryrw/MobProtID/internal/pkg/utils"
-
 	"github.com/arbitraryrw/MobProtID/internal/app/ruleparser"
-
-	"github.com/arbitraryrw/MobProtID/internal/pkg/r2handler"
-
-	"github.com/arbitraryrw/MobProtID/internal/pkg/yarahandler"
 )
 
 func main() {
-	fmt.Println("MobProtID Starting..")
-	fmt.Println(engine.Description())
-	fmt.Println(utils.Description())
+	fmt.Println("*** MobProtID Starting ***")
 
 	ruleparser.ParseRuleFile()
-
-	r2handler.Testr2()
-	yarahandler.Main()
+	engine.Start()
 }
--- internal/app/engine/engine.go
+++ internal/app/engine/engine.go
@@ -1,6 +1,23 @@
 package engine
 
+import (
+	"fmt"
+
+	"github.com/arbitraryrw/MobProtID/internal/pkg/r2handler"
+	"github.com/arbitraryrw/MobProtID/internal/pkg/yarahandler"
+)
+
 //Description Dummy function to check scope
 func Description() string {
 	return "Engine coming in!"
+}
+
+func Start() {
+	fmt.Println("[INFO] Engine Starting..")
+
+	fmt.Println("[INFO] R2 Running...")
+	r2handler.Testr2()
+
+	fmt.Println("[INFO] Yara Running...")
+	yarahandler.Main()
 }
