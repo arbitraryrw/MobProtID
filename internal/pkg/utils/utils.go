66de7ab97f3cb34581c30b5cb11821d2cdb80e52
Nikola <nikola@nglabs.local>
2020-04-12 21:04:45 +0100

    Adding missing dependencies to CI script. Also pushing basic utility classes

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -1,6 +1,46 @@
 package utils
 
+import (
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
 //Description Dummy function to check scope
 func Description() string {
 	return "utils coming in!"
+}
+
+//GetProjectRootDir returns the project root dir of MobProtID
+func GetProjectRootDir() string {
+	var _, b, _, _ = runtime.Caller(0)
+	var basePath = filepath.Dir(b)
+	splitPath := strings.Split(basePath, string(os.PathSeparator))
+
+	rulePath := "/"
+
+	for _, dir := range splitPath {
+
+		rulePath = path.Join(rulePath, dir)
+
+		if dir == "MobProtID" {
+			break
+		}
+	}
+
+	return rulePath
+}
+
+// GetWorkingDir - More info on Getwd()
+// https://golang.org/src/os/getwd.go
+func GetWorkingDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dir
 }
