095f3237cce8b1cb8977ef25245ea267fee81891
arbitraryrw <nickcucakovic@gmail.com>
2020-04-17 03:37:21 +0100

    Attempting to print current path in CI

--- internal/pkg/r2handler/r2handler_test.go
+++ internal/pkg/r2handler/r2handler_test.go
@@ -21,9 +21,10 @@ func TestUgetStringEntireBinary(t *testing.T) {
 	for _, val := range got {
 		if strings.Compare(expect, val) == 0 {
 			// Successfully found the string in the slice
-			fmt.Println(val)
 			result = true
 		}
+
+		t.Log("Found", val)
 	}
 
 	if result == false {
