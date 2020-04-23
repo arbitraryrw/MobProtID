c703ec2c800bc33047bf82ff176b7d41756c9771
arbitraryrw <nickcucakovic@gmail.com>
2020-04-23 21:51:24 +0100

    Adding util function for copying files from src to dst

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -96,3 +96,29 @@ func cleanupAnalysisDir() {
 	}
 
 }
+
+func CopyFile(src, dst string) (err error) {
+	sfi, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	if !sfi.Mode().IsRegular() {
+		// cannot copy non-regular files (directories / symlinks etc)
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
+	}
+	dfi, err := os.Stat(dst)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return
+		}
+	} else {
+		if !(dfi.Mode().IsRegular()) {
+			return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
+		}
+		if os.SameFile(sfi, dfi) {
+			return
+		}
+	}
+
+	return
+}
