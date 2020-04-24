a2359059229a8dff0998c81f1cb8e62bf66795b8
arbitraryrw <nickcucakovic@gmail.com>
2020-04-24 20:26:53 +0100

    Added unzip function to utility module and updated analysis prep logic to unzip the target binary

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"log"
@@ -15,7 +16,18 @@ import (
 var AnalysisDir string
 var AnalysisBinPath string
 
-const analysisRootDir string = "/tmp/mobprotid"
+var analysisRootDir string
+
+func init() {
+	// usr, err := user.Current()
+	// if err != nil {
+	// 	// log.Fatal(err)
+	// 	panic(err)
+	// }
+
+	// analysisRootDir = filepath.Join(usr.HomeDir, ".mobprotid")
+	analysisRootDir = "/tmp/mobprotid"
+}
 
 //Description Dummy function to check scope
 func Description() string {
@@ -115,6 +127,13 @@ func PrepBinaryForAnal(path string) {
 		fmt.Println("[ERROR]Failed to copy files:", err)
 	}
 
+	fmt.Println("Unzipping -", AnalysisBinPath, AnalysisBinPath+"-unzipped")
+	unzipErr := Unzip(AnalysisBinPath, AnalysisBinPath+"-unzipped")
+
+	if unzipErr != nil {
+		fmt.Println("[ERROR] Unable to unzip artifact for analysis", unzipErr)
+	}
+
 }
 
 func CopyFile(src, dst string) (err error) {
@@ -144,6 +163,7 @@ func CopyFile(src, dst string) (err error) {
 		}
 	}
 	err = copyFileContents(src, dst)
+
 	return
 }
 
@@ -168,4 +188,66 @@ func copyFileContents(src, dst string) (err error) {
 	}
 	err = out.Sync()
 	return
+}
+
+// Unzip takes a zip archive and unzips it to the dest
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dest, 0777)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+
+		} else {
+			os.MkdirAll(filepath.Dir(path), 0777)
+
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
