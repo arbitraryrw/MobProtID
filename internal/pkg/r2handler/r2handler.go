5de3a1a39841f275e8bf40a6262cc2cb64d3d869
arbitraryrw <nickcucakovic@gmail.com>
2020-04-30 23:00:41 +0100

    Adding jailbreak detection sample strings to check for jailbreak logic

--- internal/pkg/r2handler/r2handler.go
+++ internal/pkg/r2handler/r2handler.go
@@ -103,7 +103,6 @@ func anal() {
 	fmt.Println("Analysing", len(allBinFuncs), "binFuncs")
 
 	droidRootDetectSigs := []string{
-		"root",
 		"BusinessLogic",
 		"rootdetect",
 		"rooted",
@@ -124,7 +123,36 @@ func anal() {
 	}
 
 	jbDetectSigs := []string{
-		"cydia",
+		"/Applications/Cydia.app",
+		"/Applications/FakeCarrier.app",
+		"/Applications/Icy.app",
+		"/Applications/IntelliScreen.app",
+		"/Applications/MxTube.app",
+		"/Applications/RockApp.app",
+		"/Applications/SBSettings.app",
+		"/Applications/WinterBoard.app",
+		"/Applications/blackra1n.app",
+		"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+		"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+		"/bin/bash",
+		"/bin/sh",
+		"/etc/apt",
+		"/etc/ssh/sshd_config",
+		"/private/var/lib/apt",
+		"/private/var/lib/cydia",
+		"/private/var/mobile/Library/SBSettings/Themes",
+		"/private/var/stash",
+		"/private/var/tmp/cydia.log",
+		"/usr/bin/sshd",
+		"/usr/libexec/sftp-server",
+		"/usr/libexec/ssh-keysign",
+		"/usr/sbin/sshd",
+		"/var/cache/apt",
+		"/var/lib/apt",
+		"/var/lib/cydia",
+		"/usr/bin/cycript",
+		"/usr/local/bin/cycript",
+		"/usr/lib/libcycript.dylib",
 	}
 
 	emulatorSigs := []string{
@@ -138,6 +166,10 @@ func anal() {
 		"xposed",
 		"substrate",
 		"frida",
+		"/usr/sbin/frida-server",
+		"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+		"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+		"/Library/MobileSubstrate/MobileSubstrate.dylib",
 	}
 
 	fmt.Println(droidRootDetectSigs, jbDetectSigs, emulatorSigs, debuggerSigs, dynamicInstSigs)
