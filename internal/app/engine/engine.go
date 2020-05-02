c59cf92335dc71823c661adc5bc61f54cba8bbe1
arbitraryrw <nickcucakovic@gmail.com>
2020-05-02 22:37:30 +0100

    Parsing input file to check if it is an APK or an IPA. Literally just checking the file extension, nothing fancy.

--- internal/app/engine/engine.go
+++ internal/app/engine/engine.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"fmt"
+	"path/filepath"
 	"sync"
 
 	"github.com/arbitraryrw/MobProtID/internal/pkg/r2handler"
@@ -18,6 +19,22 @@ func Start(bp string) {
 	fmt.Println("[INFO] Engine Starting..")
 	fmt.Println("[INFO] Analysis artifacts stored: ", utils.AnalysisDir)
 
+	analFileBaseName := filepath.Base(bp)
+	platform := "unknown"
+	filesOfInterest := []string{}
+
+	if analFileBaseName[len(analFileBaseName)-4:] == ".apk" {
+		platform = "android"
+		filesOfInterest = append(filesOfInterest, ".so", ".dex")
+	} else if analFileBaseName[len(analFileBaseName)-4:] == ".ipa" {
+		platform = "ios"
+		filesOfInterest = append(filesOfInterest, ".dylib")
+	}
+
+	if platform == "unknown" {
+		panic("Engine unable to recognise file ending, must be either ipa / apk.")
+	}
+
 	utils.CreateAnalysisDir(bp)
 	utils.PrepBinaryForAnal(bp)
 
@@ -27,7 +44,6 @@ func Start(bp string) {
 
 	// parsedBinaryFilePaths = append(parsedBinaryFilePaths, bp, "/bin/bash")
 
-	filesOfInterest := []string{".so", ".dex"}
 	matchedFiles := utils.FindFilesInDir(filesOfInterest, utils.UnzippedAnalBinPath)
 	parsedBinaryFilePaths = append(parsedBinaryFilePaths, matchedFiles...)
 
