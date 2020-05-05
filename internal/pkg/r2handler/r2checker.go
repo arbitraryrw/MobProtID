4a1231ab188542225349760cd26287b1b6ef5478
arbitraryrw <nickcucakovic@gmail.com>
2020-05-05 23:14:46 +0100

    Refactoring r2checker logic to separate individual control detections

--- internal/pkg/r2handler/r2checker.go
+++ internal/pkg/r2handler/r2checker.go
@@ -7,83 +7,10 @@ import (
 	"strings"
 )
 
-var rootDetectSigs = []string{
-	"BusinessLogic",
-	"rootdetect",
-	"rooted",
-	"supersecret",
-	"/sbin/su",
-	"/system/bin/su",
-	"/system/bin/failsafe/su",
-	"/system/xbin/su",
-	"/system/xbin/busybox",
-	"/system/sd/xbin/su",
-	"/data/local/su",
-	"/data/local/xbin/su",
-	"/data/local/bin/su",
-	"/system/app/Superuser.apk",
-	"/system/etc/init.d/99SuperSUDaemon",
-	"/dev/com.koushikdutta.superuser.daemon/",
-	"/system/xbin/daemonsu",
-}
-
-var jbDetectSigs = []string{
-	"/Applications/Cydia.app",
-	"/Applications/FakeCarrier.app",
-	"/Applications/Icy.app",
-	"/Applications/IntelliScreen.app",
-	"/Applications/MxTube.app",
-	"/Applications/RockApp.app",
-	"/Applications/SBSettings.app",
-	"/Applications/WinterBoard.app",
-	"/Applications/blackra1n.app",
-	"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
-	"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
-	"/bin/bash",
-	"/bin/sh",
-	"/etc/apt",
-	"/etc/ssh/sshd_config",
-	"/private/var/lib/apt",
-	"/private/var/lib/cydia",
-	"/private/var/mobile/Library/SBSettings/Themes",
-	"/private/var/stash",
-	"/private/var/tmp/cydia.log",
-	"/usr/bin/sshd",
-	"/usr/libexec/sftp-server",
-	"/usr/libexec/ssh-keysign",
-	"/usr/sbin/sshd",
-	"/var/cache/apt",
-	"/var/lib/apt",
-	"/var/lib/cydia",
-	"/usr/bin/cycript",
-	"/usr/local/bin/cycript",
-	"/usr/lib/libcycript.dylib",
-}
-
-var emulatorSigs = []string{
-	"emulator",
-}
-
-var debuggerSigs = []string{
-	"isDebuggerConnected",
-}
-var dynamicInstSigs = []string{
-	"XposedBridge.jar",
-	"/system/framework/XposedBridge.jar",
-	"substrate",
-	"frida-agent",
-	"frida-gadget",
-	"/usr/sbin/frida-server",
-	"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
-	"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
-	"/Library/MobileSubstrate/MobileSubstrate.dylib",
-}
-
-var detectionAnalResults = make(map[string]bool, 0)
-
 //Anal - analyses the information gathered by r2
 func Anal() map[string]bool {
 
+	detectionAnalResults := make(map[string]bool, 0)
 	detectionAnalResults["jbOrRootDetection"] = false
 	detectionAnalResults["emulatorDetection"] = false
 	detectionAnalResults["debugDetection"] = false
@@ -102,7 +29,29 @@ func Anal() map[string]bool {
 		for _, bf := range f {
 			if val, ok := bf["name"]; ok {
 
-				checkAgainstSigs(val)
+				if !detectionAnalResults["jbOrRootDetection"] {
+					if checkForJBOrRoot(val) {
+						detectionAnalResults["jbOrRootDetection"] = true
+					}
+				}
+
+				if !detectionAnalResults["emulatorDetection"] {
+					if checkForEmulator(val) {
+						detectionAnalResults["emulatorDetection"] = true
+					}
+				}
+
+				if !detectionAnalResults["debugDetection"] {
+					if checkForDebugger(val) {
+						detectionAnalResults["debugDetection"] = true
+					}
+				}
+
+				if !detectionAnalResults["dniDetection"] {
+					if checkForDni(val) {
+						detectionAnalResults["dniDetection"] = true
+					}
+				}
 
 				// Function name
 				// fmt.Println("[DEBUG]", val)
@@ -117,8 +66,29 @@ func Anal() map[string]bool {
 
 		for _, s := range v {
 
-			checkAgainstSigs(s)
+			if !detectionAnalResults["jbOrRootDetection"] {
+				if checkForJBOrRoot(s) {
+					detectionAnalResults["jbOrRootDetection"] = true
+				}
+			}
 
+			if !detectionAnalResults["emulatorDetection"] {
+				if checkForEmulator(s) {
+					detectionAnalResults["emulatorDetection"] = true
+				}
+			}
+
+			if !detectionAnalResults["debugDetection"] {
+				if checkForDebugger(s) {
+					detectionAnalResults["debugDetection"] = true
+				}
+			}
+
+			if !detectionAnalResults["dniDetection"] {
+				if checkForDni(s) {
+					detectionAnalResults["dniDetection"] = true
+				}
+			}
 		}
 	}
 
@@ -126,7 +96,30 @@ func Anal() map[string]bool {
 	for k, v := range allSymbolsInBinary {
 		fmt.Println("[INFO] File ->", k)
 		for _, s := range v {
-			checkAgainstSigs(s)
+
+			if !detectionAnalResults["jbOrRootDetection"] {
+				if checkForJBOrRoot(s) {
+					detectionAnalResults["jbOrRootDetection"] = true
+				}
+			}
+
+			if !detectionAnalResults["emulatorDetection"] {
+				if checkForEmulator(s) {
+					detectionAnalResults["emulatorDetection"] = true
+				}
+			}
+
+			if !detectionAnalResults["debugDetection"] {
+				if checkForDebugger(s) {
+					detectionAnalResults["debugDetection"] = true
+				}
+			}
+
+			if !detectionAnalResults["dniDetection"] {
+				if checkForDni(s) {
+					detectionAnalResults["dniDetection"] = true
+				}
+			}
 		}
 	}
 
@@ -211,39 +204,133 @@ func Anal() map[string]bool {
 	return detectionAnalResults
 }
 
-func checkAgainstSigs(s string) {
+func checkForJBOrRoot(s string) bool {
+
+	var rootDetectSigs = []string{
+		"BusinessLogic",
+		"rootdetect",
+		"rooted",
+		"supersecret",
+		"/sbin/su",
+		"/system/bin/su",
+		"/system/bin/failsafe/su",
+		"/system/xbin/su",
+		"/system/xbin/busybox",
+		"/system/sd/xbin/su",
+		"/data/local/su",
+		"/data/local/xbin/su",
+		"/data/local/bin/su",
+		"/system/app/Superuser.apk",
+		"/system/etc/init.d/99SuperSUDaemon",
+		"/dev/com.koushikdutta.superuser.daemon/",
+		"/system/xbin/daemonsu",
+	}
+
+	var jbDetectSigs = []string{
+		"/Applications/Cydia.app",
+		"/Applications/FakeCarrier.app",
+		"/Applications/Icy.app",
+		"/Applications/IntelliScreen.app",
+		"/Applications/MxTube.app",
+		"/Applications/RockApp.app",
+		"/Applications/SBSettings.app",
+		"/Applications/WinterBoard.app",
+		"/Applications/blackra1n.app",
+		"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+		"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+		"/bin/bash",
+		"/bin/sh",
+		"/etc/apt",
+		"/etc/ssh/sshd_config",
+		"/private/var/lib/apt",
+		"/private/var/lib/cydia",
+		"/private/var/mobile/Library/SBSettings/Themes",
+		"/private/var/stash",
+		"/private/var/tmp/cydia.log",
+		"/usr/bin/sshd",
+		"/usr/libexec/sftp-server",
+		"/usr/libexec/ssh-keysign",
+		"/usr/sbin/sshd",
+		"/var/cache/apt",
+		"/var/lib/apt",
+		"/var/lib/cydia",
+		"/usr/bin/cycript",
+		"/usr/local/bin/cycript",
+		"/usr/lib/libcycript.dylib",
+	}
+
 	for _, ds := range rootDetectSigs {
 		if strings.Contains(strings.ToLower(s), ds) {
 			fmt.Println("[FINDING] Root Detection - We have a match!", ds, "was in", s)
 
-			detectionAnalResults["jbOrRootDetection"] = false
+			return true
 		}
 	}
 
 	for _, ds := range jbDetectSigs {
 		if strings.Contains(strings.ToLower(s), ds) {
 			fmt.Println("[FINDING] Jailbreak Detection - We have a match!", ds, "was in", s)
-			detectionAnalResults["jbOrRootDetection"] = false
+
+			return true
 		}
 	}
+
+	return false
+}
+
+func checkForEmulator(s string) bool {
+	var emulatorSigs = []string{
+		"emulator",
+	}
+
 	for _, ds := range emulatorSigs {
 		if strings.Contains(strings.ToLower(s), ds) {
 			fmt.Println("[FINDING] Emulator Detection - We have a match!", ds, "was in", s)
 
-			detectionAnalResults["emulatorDetection"] = false
+			return true
 		}
 	}
+
+	return false
+}
+
+func checkForDebugger(s string) bool {
+
+	var debuggerSigs = []string{
+		"isDebuggerConnected",
+	}
+
 	for _, ds := range debuggerSigs {
 		if strings.Contains(strings.ToLower(s), ds) {
 			fmt.Println("[FINDING] Debugger Instrumentation Detection - We have a match!", ds, "was in", s)
-			detectionAnalResults["debugDetection"] = false
+
+			return true
 		}
+	}
+	return false
+}
+
+func checkForDni(s string) bool {
+
+	var dynamicInstSigs = []string{
+		"XposedBridge.jar",
+		"/system/framework/XposedBridge.jar",
+		"substrate",
+		"frida-agent",
+		"frida-gadget",
+		"/usr/sbin/frida-server",
+		"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+		"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+		"/Library/MobileSubstrate/MobileSubstrate.dylib",
 	}
 
 	for _, ds := range dynamicInstSigs {
 		if strings.Contains(strings.ToLower(s), ds) {
 			fmt.Println("[FINDING] Dynamic Instrumentation Detection - We have a match!", ds, "was in", s)
-			detectionAnalResults["dniDetection"] = false
+
+			return true
 		}
 	}
+
+	return false
 }
