17af30b1a9ea7288cd1618a43cb11e6816588c3b
arbitraryrw <nickcucakovic@gmail.com>
2020-05-13 23:16:03 +0100

    Preparing rule parsing logic for json rule files

--- internal/app/ruleparser/ruleparser.go
+++ internal/app/ruleparser/ruleparser.go
@@ -5,24 +5,50 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/arbitraryrw/MobProtID/internal/pkg/utils"
 )
 
 func ParseRuleFile() {
 	fmt.Println("Parsing rule file..")
 
-	jsonFile, err := os.Open("/home/nikola/projects/MobProtID/rules/test.json")
+	var ruleFiles []string
+	ruleDir := path.Join(utils.GetProjectRootDir(), "rules/")
+
+	err := filepath.Walk(ruleDir, func(path string, info os.FileInfo, err error) error {
+
+		if strings.Contains(filepath.Base(path), "_rules.json") {
+			ruleFiles = append(ruleFiles, path)
+		}
+
+		return nil
+	})
 
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
+	for _, file := range ruleFiles {
+		fmt.Println("[INFO] Analysing rule file:", file)
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+		jsonFile, err := os.Open(file)
 
-	var result map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	fmt.Println("rules:", result["users"])
+		// defer the closing of our jsonFile so that we can parse it later on
+		defer jsonFile.Close()
+
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		var result map[string]interface{}
+		json.Unmarshal([]byte(byteValue), &result)
+
+		fmt.Println("rules:", result)
+	}
+
 }
