ea6790a91128048c3dd7f652d5936b295a03615f
arbitraryrw <nickcucakovic@gmail.com>
2020-05-25 17:59:04 +0100

    Bottom up unit tests added for rule evaluation logic in rule parser

--- internal/app/ruleparser/ruleparser_test.go
+++ internal/app/ruleparser/ruleparser_test.go
@@ -25,14 +25,34 @@ func TestParseRuleFile(t *testing.T) {
 	}
 }
 
-func TestEvalRule(t *testing.T) {
+func TestEvalRulePositive(t *testing.T) {
+	var expect = true
+	var r Rule
+
+	var sigs []interface{}
+	sigs = append(sigs, "test1", "test2", "test3")
+
+	r.Handler = "dummyTestHandlerPass"
+	r.MatchType = "test"
+	r.MatchValue = sigs
+	r.Type = "test"
+
+	var got = evalRule(r)
+
+	if got != expect {
+		fmt.Println("Failed comparison!")
+		t.Errorf("evalRule() = %t; want %t", got, expect)
+	}
+}
+
+func TestEvalRuleNegative(t *testing.T) {
 	var expect = false
 	var r Rule
 
 	var sigs []interface{}
 	sigs = append(sigs, "test1", "test2", "test3")
 
-	r.Handler = "test"
+	r.Handler = "dummyTestHandlerFail"
 	r.MatchType = "test"
 	r.MatchValue = sigs
 	r.Type = "test"
