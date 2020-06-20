5026993ad92121d4649d20e0c8ca173f7fbc8cab
arbitraryrw <nickcucakovic@gmail.com>
2020-06-20 19:47:28 +0100

    yarahandler: unit testing yara pre-analysis logic

--- internal/pkg/yarahandler/yarahandler_test.go
+++ internal/pkg/yarahandler/yarahandler_test.go
@@ -1,3 +1,46 @@
 package yarahandler
 
-// ToDo: Unit test wrapper code around yara when it starts being developed
+import (
+	"strings"
+	"sync"
+	"testing"
+)
+
+func init() {}
+
+func TestPrepareAnal(t *testing.T) {
+
+	analFile := "/bin/ls"
+
+	parsedBinaryFilePaths := make([]string, 0)
+	parsedBinaryFilePaths = append(parsedBinaryFilePaths, analFile)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go PrepareAnal(parsedBinaryFilePaths, &wg)
+	wg.Wait()
+
+	for file, yaraBundle := range yaraAnalysisBundle {
+
+		if file != analFile {
+			t.Errorf(
+				"PrepareAnal = yara analysis bundle malformed, expected %q got %q",
+				analFile,
+				file)
+		}
+
+		for rule := range yaraBundle {
+
+			if !strings.Contains(rule, ".yara") {
+				t.Errorf(
+					"PrepareAnal = yara analysis bundle malformed, expected %q got %q in name",
+					rule,
+					".yara")
+			}
+
+		}
+
+	}
+
+}
