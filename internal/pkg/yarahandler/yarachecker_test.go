e3932cd642bd6abdcb6847b364eae0572dde703c
arbitraryrw <nickcucakovic@gmail.com>
2020-06-22 21:34:55 +0100

    yarahandler: unit tests expanded to validate the rule result for expected values

--- internal/pkg/yarahandler/yarachecker_test.go
+++ internal/pkg/yarahandler/yarachecker_test.go
@@ -11,7 +11,6 @@ import (
 func init() {}
 
 func TestHandleRule(t *testing.T) {
-
 	sampleBinaryRelPath := "../../../test/sample_binary"
 	sampleBinAbsPath, err := filepath.Abs(sampleBinaryRelPath)
 
@@ -38,6 +37,8 @@ func TestHandleRule(t *testing.T) {
 	r.MatchType = "regex"
 	r.MatchValue = sigs
 	r.Type = "ruleName"
+	r.Name = "part_1"
+	r.Invert = false
 
 	result := HandleRule(r)
 
@@ -47,6 +48,47 @@ func TestHandleRule(t *testing.T) {
 			true,
 			result.Match)
 	}
+
+	if len(result.Evidence) != 1 {
+		t.Errorf(
+			"HandleRule = yara rule result evidence missmatch"+
+				", expected %q match, got: %q match",
+			1,
+			len(result.Evidence))
+	}
+
+	for _, e := range result.Evidence {
+		if e.File != sampleBinAbsPath {
+			t.Errorf(
+				"HandleRule = yara rule result evidence file missmatch"+
+					", expected %q, got: %q",
+				sampleBinAbsPath,
+				e.File)
+		}
+
+		if e.RuleName != r.Name {
+			t.Errorf(
+				"HandleRule = yara rule result evidence rule name missmatch"+
+					", expected %q, got: %q",
+				r.Name,
+				e.RuleName)
+		}
+		if e.Name != "It's MobProtID here!" {
+			t.Errorf(
+				"HandleRule = yara rule result evidence name missmatch"+
+					", expected %q, got: %q",
+				"It's MobProtID here!",
+				e.Name)
+		}
+		if e.Offset != "1918" {
+			t.Errorf(
+				"HandleRule = yara rule result evidence offset"+
+					"missmatch, expected %q, got: %q",
+				"1918",
+				e.Offset)
+		}
+	}
+
 }
 
 func TestUcreateEvidenceStruct(t *testing.T) {
